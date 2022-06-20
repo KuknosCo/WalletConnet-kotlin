4d01a95a3ad55641f4e5a02365232a7877205a4b
siavash aghablaee <siavash.aghabalaee@gmail.com>
2022-06-20 16:34:57 +0430

    android function interface

---
+++ walletconnect/src/main/java/com/kuknos/walletconnect/walletconnectmodels/JavaScriptInterface.kt
@@ -0,0 +1,17 @@
+package com.kuknos.walletconnect.walletconnectmodels
+
+import android.content.Context
+import android.util.Log
+import android.webkit.JavascriptInterface
+import android.widget.Toast
+import com.kuknos.walletconnect.WalletConnect
+
+class JavaScriptInterface (var context: Context,var publicKey : String,var walletConnect : WalletConnect) {
+    @JavascriptInterface
+    fun connectRequest(data: String?) {
+        Log.i("KuknosLog","data : "+data)
+        data?.let {
+            walletConnect.connect(it,publicKey,context)
+        }
+    }
+}
